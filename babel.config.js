98104d72bf481117f4a733a525adcc6d0cc018c7
Ignas <nionis0697@gmail.com>
2021-03-13 15:58:16 +0200

    prepare eslint

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,31 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
 
   plugins: [
     [
-      'module-resolver',
+      "module-resolver",
       {
-        root: ['.'],
-        extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+        root: ["."],
+        extensions: [
+          ".ios.ts",
+          ".android.ts",
+          ".ts",
+          ".ios.tsx",
+          ".android.tsx",
+          ".tsx",
+          ".jsx",
+          ".js",
+          ".json",
+        ],
         alias: {
-          '#components': './app/components',
-          '#hooks': './app/hooks',
-          '#screens': './app/screens',
-          '#contexts': './app/contexts',
-          '#navigation': './app/navigation',
-          '#theme': './app/theme',
-          '#assets': './app/assets',
-          '#utils': './app/utils',
+          "#components": "./app/components",
+          "#hooks": "./app/hooks",
+          "#screens": "./app/screens",
+          "#contexts": "./app/contexts",
+          "#navigation": "./app/navigation",
+          "#theme": "./app/theme",
+          "#assets": "./app/assets",
+          "#utils": "./app/utils",
         },
       },
     ],
