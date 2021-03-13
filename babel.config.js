7ca40140566d3b54c65312b0288819a66a6e8074
Ignas <nionis0697@gmail.com>
2021-03-13 13:02:28 +0200

    dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          'components': './app/components',
+          'hooks': './app/hooks',
+          'screens': './app/screens',
+          'contexts': './app/contexts',
+          'navigation': './app/navigation',
+          'theme': './app/theme',
+          'assets': './app/assets',
+          'utils': './app/utils',
+        },
+      },
+    ],
+  ],
 };
